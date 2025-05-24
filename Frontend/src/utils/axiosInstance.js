fa26486ac5d24889c8b1303a1c4cfbe3bc5c6447
Chirag-Solanki6804 <crs279836@gmail.com>
2025-05-24 08:55:31 +0530

    added production backend url

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const PRO_BASE_URL = "https://devskillz.onrender.com/api/v1";
+const PRO_BASE_URL = "https://task-tracker-bgh4.onrender.com/api/v1";
 
 const DEV_BASE_URL = "http://localhost:3000/api/v1";
 
 const axiosInstance = axios.create({
-  baseURL: DEV_BASE_URL,
+  baseURL: PRO_BASE_URL,
   withCredentials: true,
 });
 
